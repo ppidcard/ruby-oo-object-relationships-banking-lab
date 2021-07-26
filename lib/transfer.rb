16f00b9470f6cd46cf684af905d6737565e2b6da
ppidcard <tvxqlikei@live.cn>
2021-07-27 08:00:16 +1000

    Done

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,36 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :amount, :status
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if ((status == 'pending' && sender.balance > amount) && (sender.valid? && receiver.valid?)) 
+    sender.balance -= amount
+    receiver.deposit(amount)
+    self.status = 'complete'
+    else 
+    self.status = 'rejected'  
+    "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if(self.status == 'complete')
+      sender.deposit(amount)
+      receiver.balance -= amount
+      self.status = 'reversed'
+    else
+      'rejected'
+    end
+  end
+
 end
